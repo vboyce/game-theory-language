fe0d2ca959669c46384edf82d111b7b5085bc580
Veronica Boyce <vboyce@mit.edu>
2022-06-14 17:42:36 -0700

    add spiking version, and split PD variants

--- experiments/server/main.js
+++ experiments/server/main.js
@@ -12,6 +12,11 @@ function createRoles(players) {
   return roles;
 }
 
+function compareNumbers(a, b) //deal with the fact that js defaults to lexicographic sorting....
+{
+    return a - b;
+}
+
 function chooseTargets(targets){
   //takes a list of flower objects, a list of utilties and a number to select
   //returns the context
@@ -26,6 +31,44 @@ function createPDRewards(){
   //sucker payoff fixed at 0
   // other rewards on 1-9, but have to respect ordering win > coop > defect 
   var a=_.slice(_.shuffle(_.range(1,10)),0,3).sort()
+  var coop=a[1]
+  var defect=a[0]
+  var win=a[2]
+  var payoffs={
+   "AA":{p1:coop,p2:coop},
+   "AB":{p1:0,p2:win},
+   "BA":{p1:win,p2:0},
+   "BB":{p1:defect,p2:defect}
+  }
+  return payoffs
+}
+function createEasyPDRewards(){
+  //sucker payoff fixed at 0
+  // other rewards on 1-12, but have to respect ordering win > coop > defect 
+  // we redraw until we have a set where 2*coop \ge win 
+  var a=_.slice(_.shuffle(_.range(1,13)),0,3).sort(compareNumbers)
+  while (2*a[1]<a[2]){
+    a=_.slice(_.shuffle(_.range(1,13)),0,3).sort(compareNumbers)
+  }
+  var coop=a[1]
+  var defect=a[0]
+  var win=a[2]
+  var payoffs={
+   "AA":{p1:coop,p2:coop},
+   "AB":{p1:0,p2:win},
+   "BA":{p1:win,p2:0},
+   "BB":{p1:defect,p2:defect}
+  }
+  return payoffs
+}
+function createHardPDRewards(){
+  //sucker payoff fixed at 0
+  // other rewards on 1-12, but have to respect ordering win > coop > defect 
+  // we redraw until we have a set where 2*coop < win 
+  var a=_.slice(_.shuffle(_.range(1,13)),0,3).sort(compareNumbers)
+  while (2*a[1]>=a[2]){
+    a=_.slice(_.shuffle(_.range(1,13)),0,3).sort(compareNumbers)
+  }
   var coop=a[1]
   var defect=a[0]
   var win=a[2]
@@ -54,19 +97,49 @@ function createBoSRewards(){
   return payoffs
 }
 
+function createSpikeBoSRewards(){
+  //off diagonal fixed at 0
+  // other rewards have one from 25-30 and one from 3-7
+  var best=_.shuffle(_.range(25,31))[0]
+  var okay=_.shuffle(_.range(3,8))[0]
+  var miss=0
+  var payoffs={
+   "AA":{p1:best,p2:okay},
+   "AB":{p1:miss,p2:miss},
+   "BA":{p1:miss,p2:miss},
+   "BB":{p1:okay,p2:best}
+  }
+  return payoffs
+}
+
+
 function createRewards(type){
   //wrapper
   if (type=="PD") return createPDRewards()
   if (type=="BoS") return createBoSRewards()
+  if (type=="spikeBoS") return createSpikeBoSRewards()
+  if (type=="easyPD") return createEasyPDRewards()
+  if (type=="hardPD") return createHardPDRewards()
 }
 
-function getType(game){
+function getType(game, repNum){
   //wrapper
   if (game=="PD") return "PD"
   if (game=="BoS") return "BoS"
   if (game=="mixPDBoS") {
     if(Math.random()>.5) return "PD"
     else return "BoS"
+  }
+  if (game=="spikeMix"){
+    const spiked=[6,15,22,31] // hard code with 0-indexed rounds get spiked-BoS distribution
+    if (spiked.includes(repNum)) return "spikeBoS"
+    else {
+      r=Math.random()
+      if (r<.44) return "BoS" // distribution is (counting spikes), .5 BoS, .25 each PD variety
+      else if (r<.72) return "easyPD" 
+      else return "hardPD"
+    }
+
   }
 }
 
@@ -116,9 +189,12 @@ Empirica.gameInit((game, treatment) => {
         //mixed_targets=_.shuffle(targets)
       // Loop through targets in block   
         const round = game.addRound();
-        round.set('type', getType(treatment.gameType))
+        round.set('type', getType(treatment.gameType, repNum))
         round.set('targets', chooseTargets(targets));
         round.set('payoffs', createRewards(round.get('type')));
+        console.log(repNum)
+        console.log(round.get('type'))
+        console.log(round.get('payoffs'))
         round.set('repNum', repNum)        
         round.addStage({
           name: "selection",
